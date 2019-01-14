58900f4b6d98395f82ff3a5f81027a895af33dfe
Alicia Hetrick <44244701+ahetrick22@users.noreply.github.com>
2019-01-14 14:54:30 -0500

    Update rootReducer.js

--- client/src/Reducers/rootReducer.js
+++ client/src/Reducers/rootReducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 import boardReducer from './boardReducer';
 import cardReducer from './cardReducer';
 import orgReducer from './orgReducer';
-import { listReducer } from "./listReducer";
+import listReducer from "./listReducer";
 
 
 const rootReducer = combineReducers({
