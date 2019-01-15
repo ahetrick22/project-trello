d9f187cd31019ca372bb9616b52f9cdfea27893f
Alicia Hetrick <ahetrick22@gmail.com>
2019-01-15 13:59:04 -0500

    start to render board component with data from the GET /board/:id endpoint

--- client/src/Actions/types.js
+++ client/src/Actions/types.js
@@ -11,7 +11,7 @@ export const LOGIN = 'LOGIN';
 export const FETCH_BOARDS = 'FETCH_BOARDS';
 export const FETCH_ORG = 'FETCH_ORG';
 //for the board reducer
-export const FETCH_LISTS = 'FETCH_LISTS';
+export const FETCH_BOARD = 'FETCH_BOARD';
 //for the list reducer
 export const FETCH_CARDS = 'FETCH_CARDS';
 //for the card reducer
