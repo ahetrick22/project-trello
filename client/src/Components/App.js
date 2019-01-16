bd2824c7e8fcf3f5a2f96cbda9463ed6dfc8478a
Edward Barton <edward.barton@icloud.com>
2019-01-16 11:47:47 -0500

    style org home page

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import * as actions from '../Actions';
 import { connect } from 'react-redux';
+import { TYPEFACE } from '../css/StyleGuide';
 
 class App extends React.Component {
   render() {
     return (
       <div
         className="app"
-        style={{ boxSizing: 'border-box', marginTop: '40px' }}
+        style={{
+          boxSizing: 'border-box',
+          marginTop: '40px',
+          fontFamily: TYPEFACE
+        }}
       >
         {this.props.children}
       </div>
