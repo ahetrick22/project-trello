bad70d9ddb703390823781fddc86bf2422ea7462
Alicia Hetrick <ahetrick22@gmail.com>
2019-01-16 10:05:33 -0500

    basics of working login and register routes with jwt authentication, still needs more validation but is functional for now

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const hardCodedData = require('./routes/hard-coded-data');
 const organizationRoutes = require('./routes/organizations');
 const boardRoutes = require('./routes/boards');
 const cardRoutes = require('./routes/cards');
-//const loginRoutes = require('./routes/login');
+const loginRoutes = require('./routes/login');
 const listRoutes = require('./routes/lists');
 
 app.use(mainRoutes)
@@ -30,7 +30,7 @@ app.use(hardCodedData);
 app.use(organizationRoutes);
 app.use(boardRoutes);
 app.use(cardRoutes);
-//app.use(loginRoutes);
+app.use(loginRoutes);
 app.use(listRoutes);
 
 const server = http.createServer(app);
