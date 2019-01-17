b0a4c6f83888234678c75d29b2e5d9bce9e3a3fc
Matt Fine <mfine@wesleyan.edu>
2019-01-16 21:55:03 -0500

    Updated card route to account for activity data. comment is now broken due to authentication. will fix tomorrow

--- routes/cards.js
+++ routes/cards.js
@@ -5,68 +5,76 @@ router.get('/card/:id', (req, res) => {
    //make sure it's a valid ID and won't trigger a cast error
    if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
     //then find the matching card
-    Card.findById(req.params.id, (err, card) => {
-      if(err) throw err;
-      //if no card at that id, then tell the user
-      if (!card) {
-        res.send(404, 'No card with that id');
-      //otherwise, populate the card with its comments and send it
-      } else {
-        Card.findById(req.params.id, (err, fullCard) => 
-        fullCard.populate('comments', () => {
-          if(err) throw err;
-          res.send(JSON.stringify(fullCard));
+        Card.findById(req.params.id, (err, card) => {
+        if(err) throw err;
+        //if no card at that id, then tell the user
+        if (!card) {
+            res.send(404, 'No card with that id');
+        //otherwise, populate the card with its comments and send it
+        } else {
+          Card.findById(req.params.id, (err, fullCard) => 
+            fullCard.populate('comments', () => {
+                if(err) throw err;
+                res.send(JSON.stringify(fullCard));
+            })
+          )}
         })
-        )}
-    })
-  //if the object ID wasn't in the correct format
-  } else {
-    res.send(400, 'Send a valid object ID as a parameter');
-  }
+    //if the object ID wasn't in the correct format
+    } else {
+      res.send(400, 'Send a valid object ID as a parameter');
+    }
 })
 
 router.put('/card/:id', (req, res) => {
   //check to see which params come in the body
   if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
+    User.find({email : req.headers.email}, (err, user) => {
+        if (err) throw err;
+        let activityObj = {
+            user: user._id,
+            text: `${user.email} `,
+            timestamp: new Date()
+        };
+    })
+
       let updateObject = {};
       if(req.body.title) {
           updateObject.title = req.body.title;
+          activityObj.text += `changed the title to ${req.body.title}`
       }
 
       if (req.body.label) {
           updateObject.label = req.body.label;
+          activityObj.text += `changed the label to ${req.body.label}`
       }
 
       if (req.body.description) {
           updateObject.description = req.body.description;
+          activityObj.text += 'changed the description'
       }
 
       if (req.body.archived) {
         updateObject.archived = req.body.archived;
+        activityObj.text += 'archived the post'
       }
 
       if (req.body.list) {
           updateObject.list = req.body.list;
+          activityObj.text += `moved this card to ${req.body.list}`
       }
-
-    //   if (req.body.activity) {
-          
-    //   }
-      
+        
       if (Object.keys(updateObject).length === 0) {
           res.send(400, "Body must have parameters matching parameters");
       }
 
       let { id } = req.params;
-      User.find({ email : req.headers.email }, (err, user) => {
-          if (err) throw err;
-          
-      })
+    
       Card.findByIdAndUpdate(id, updateObject,(err, card) => {
           if(err) throw err;
           if (!card) {
               res.send(404, 'no card with that id');
           } else {
+              card.activity.push(activityObj);
               Card.findById(id).populate({
                   path: 'comments'
               }).exec((err, fullCard) => {
