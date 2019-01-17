884f6bae5e024eab44e09f7c586911f0497d02fd
Alicia Hetrick <ahetrick22@gmail.com>
2019-01-17 12:51:31 -0500

    fix join dirname

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ if (process.env.NODE_ENV === 'production') {
   // if it doesn't recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
